b4d180101b284ba9173eb9c8c2db09188d6adb74
dattran01674158329-lgtm <dattran01674158329@gmail.com>
2026-04-15 17:50:23 +0700

    feat: format output to JSON for 2 channels

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -3,14 +3,29 @@
  * Mon: Mang Cam Bien - Tuan 03
  */
 
-int adcValues[2]; float nhietDo[2]; char chuoi[30];
+int adcValues[2]; 
+float nhietDo[2]; 
+
+void setup() {
+  // Khoi tao Serial voi baudrate 9600
+  Serial.begin(9600);
+  delay(100);
+}
 
 void loop() {
-  adcValues[0] = analogRead(A0);
-  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
-  adcValues[1] = analogRead(A1);
-  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
-  sprintf(chuoi, "%d,%d\n", (int)nhietDo[0], (int)nhietDo[1]);
-  Serial.print(chuoi);
-  delay(100);
+  // Sinh vien B van giu nguyen logic doc 2 kenh A0 va A1
+  for(int i = 0; i < 2; i++) {
+    adcValues[i] = analogRead(A0 + i);
+    nhietDo[i] = (adcValues[i] * 500.0) / 1023.0;
+  }
+
+  // SỬA CODE: Thay vi dung sprintf in ra chuoi "25,30"
+  // Chung ta in ra JSON: {"T0": 25.5, "T1": 30.2}
+  Serial.print("{\"T0\":"); 
+  Serial.print(nhietDo[0], 1); // In 1 chu so thap phan
+  Serial.print(",\"T1\":"); 
+  Serial.print(nhietDo[1], 1);
+  Serial.println("}");
+  
+  delay(1000); // Doc moi giay 1 lan
 }
